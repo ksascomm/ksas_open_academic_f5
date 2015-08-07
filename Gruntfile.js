bae0745cc62443384bfc6831c5bf3a8b93c2ea1e
ksascomm <ksasweb@jhu.edu>
2015-08-07 11:47:38 -0400

    migrated theme frontend from compass to grunt

---
+++ Gruntfile.js
@@ -0,0 +1,102 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    //Sass Compiling Task
+    sass: {
+      options: {
+        includePaths: ['assets/bower_components/foundation/scss']
+      },
+      dev: {
+        options: {
+          style: 'expanded',
+          sourceMap: true,
+        },
+        files: {
+          'assets/stylesheets/app.css': 'assets/scss/app.scss'
+        }
+      },
+      dist: {
+        options: {
+          outputStyle: 'compressed',
+          sourceMap: true,
+        },
+        files: {
+          'assets/stylesheets/app.min.css': 'assets/scss/app.scss'
+        }
+      }
+    },
+
+    //autoprefixer
+    postcss: {
+      options: {
+        map: true,
+        processors: [
+          require('autoprefixer-core')({browsers: ['last 2 versions', 'ie 8', 'ie 9', '> 1%']})
+        ]
+      },
+      //prefix all files
+      multiple_files: {
+        expand: true,
+        flatten: true,
+        src:'assets/stylesheets/*.css',
+        dest:'assets/stylesheets/',
+      }
+    },
+
+    //imagemin
+    imagemin: {
+       dist: {
+          options: {
+            optimizationLevel: 5
+          },
+          files: [{
+             expand: true,
+             cwd: 'assets/images',
+             src: ['**/*.{png,jpg,gif}'],
+             dest: 'assets/images'
+          }]
+       }
+    },
+    //browserSync
+    browserSync: {
+        dev: {
+            bsFiles: {
+                    src : [
+                        'assets/stylesheets/*.css',
+                        '**/*.php',
+                        'assets/js/vendor/*.js',
+                        'assets/js/*.js',
+                        'assets/images/**/*.{png,jpg,jpeg,gif,webp,svg}'
+                    ]
+            },
+            options: {
+                watchTask: true,
+                proxy: "bicycle.dev"
+            }
+        }
+    },
+
+    //Watch Task
+    watch: {
+      grunt: {
+        options: {
+          reload: true
+        },
+        files: ['Gruntfile.js']
+      },
+      sass: {
+        files: 'assets/scss/**/*.scss',
+        tasks: ['sass', 'postcss', 'imagemin'],
+      },
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.registerTask('build', ['sass']);
+  grunt.registerTask('default', ['sass','browserSync','watch']);
+}
