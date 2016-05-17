72aa43a61e1fa5c6da98459b12b7430b8319b681
ksascomm <ksasweb@jhu.edu>
2016-05-17 11:31:44 -0400

    browsehappy link if browser older than IE10

--- header.php
+++ header.php
@@ -21,6 +21,9 @@
 	        <script async src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
 	        <script async src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
 	        <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() ?>/assets/stylesheets/app.ie.css">
+		<div data-alert class="alert-box alert">
+        <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.'); ?>	
+        </div>	
 	    <![endif]-->
 
 	<!-- Modernizr and Jquery Script -->
