9de3baa393eb1ca02f5565d9e7d3d38f3c70dc71
ksascomm <ksasweb@jhu.edu>
2016-05-26 14:13:52 -0400

    IE conditional updates

--- header.php
+++ header.php
@@ -15,16 +15,17 @@
   <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/stylesheets/app.min.css">
 
   <!-- CSS Files: Conditionals -->
-	 <!-- IE Fix for HTML5 Tags -->
-	    <!--[if lt IE 10]>
-	        <script async src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-	        <script async src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
-	        <script async src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
-	        <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() ?>/assets/stylesheets/app.ie.css">
+	<!-- Make IE a modern browser -->
+	<!--[if IE]>
+		<script src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/css3-mediaqueries/0.1/css3-mediaqueries.min.js"></script>
+	<![endif]-->
+  	<!--[if lt IE 11]>
+		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() ?>/assets/css/app.ie.css">
 		<div data-alert class="alert-box alert">
-        <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.'); ?>	
-        </div>	
-	    <![endif]-->
+		<?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.'); ?>	
+		</div>		
+	<![endif]-->
 
 	<!-- Modernizr and Jquery Script -->
   <?php wp_enqueue_script('jquery'); ?>
