4cdc65c0d0c01241e518a7803818e178062e4874
ksascomm <ksasweb@jhu.edu>
2016-06-23 15:55:49 -0400

    swapped second author & purchase link

--- single-category-books.php
+++ single-category-books.php
@@ -11,24 +11,22 @@
 		<li><?php if ( get_post_meta($post->ID, 'ecpt_pub_date', true) ) : echo get_post_meta($post->ID, 'ecpt_pub_date', true);  endif; ?>
 		<?php if ( get_post_meta($post->ID, 'ecpt_publisher', true) ) :?>, <?php echo get_post_meta($post->ID, 'ecpt_publisher', true);  endif; ?></li>
 		<li>
-			<strong>
-				<a href="<?php echo get_permalink($faculty_post_id); ?>"><?php echo get_the_title($faculty_post_id); ?> 
-				<?php if ( get_post_meta($post->ID, 'ecpt_pub_role', true)) :?>, <?php echo get_post_meta($post->ID, 'ecpt_pub_role', true); endif; ?>
-				</a>
-			</strong>
+			<a href="<?php echo get_permalink($faculty_post_id); ?>"><?php echo get_the_title($faculty_post_id); ?> 
+			<?php if ( get_post_meta($post->ID, 'ecpt_pub_role', true)) :?>, <?php echo get_post_meta($post->ID, 'ecpt_pub_role', true); endif; ?>
+			</a>
 		</li>
+		<?php if (get_post_meta($post->ID, 'ecpt_author_cond', true) == 'on') { ?>
+		<li>
+			<a href="<?php echo get_permalink($faculty_post_id2); ?>"><?php echo get_the_title($faculty_post_id2); ?>, <?php echo get_post_meta($post->ID, 'ecpt_pub_role2', true); ?>
+			</a>
+		</li>
+		<?php } ?>
 		<li><?php if ( get_post_meta($post->ID, 'ecpt_pub_link', true) ) :?> 
 				<a href="http://<?php echo get_post_meta($post->ID, 'ecpt_pub_link', true); ?>">
 					Purchase Online <span class="fa fa-external-link-square"></span>
 				</a>
 			<?php endif; ?>
 		</li>
-		<?php if (get_post_meta($post->ID, 'ecpt_author_cond', true) == 'on') { ?>
-		<li>
-			<a href="<?php echo get_permalink($faculty_post_id2); ?>"><?php echo get_the_title($faculty_post_id2); ?>,&nbsp;<?php echo get_post_meta($post->ID, 'ecpt_pub_role2', true); ?>
-			</a>
-		</li>
-		<?php } ?>
 	</ul>
 	
 	<?php the_content(); ?>		
